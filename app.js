bf2c542678b5d469980b0757fc90c8e08ca97afc
Leandro Tomaz Andre <tqi_landre@uolinc.com>
2015-11-05 13:56:30 -0200

    Adicionando user.js em models e corrigindo problemas nas dependências do express (session, cookie-parser e bodyParser)

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var load = require('express-load');
 var cookieParser = require('cookie-parser');
 var expressSession = require('express-session');
+var bodyParser = require('body-parser');
 var app = express();
 
 // view engine setup
@@ -9,10 +10,19 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 
-app.use(express.cookieParser('ntalk'));
-app.use(express.session());
-app.use(express.json());
-app.use(express.urlencoded());
+app.use(cookieParser('ntalk'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(expressSession({
+	secret: 'sbrubles',
+    name: 'ntalk',
+//    store: sessionStore, // connect-mongo session store
+    proxy: true,
+    resave: true,
+    saveUninitialized: true
+}));
 app.use(express.static(__dirname + 'public'));
 
 load('models').then('controllers').then('routes').into(app);
---
+++ models/usuario.js
@@ -0,0 +1,3 @@
+module.exports = function(app) {
+	var usuario = {nome: "", email: ""}
+}
